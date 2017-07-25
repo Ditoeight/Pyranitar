af1fa12e54fddf54d3bf7cabc6569d9098f33849
Tyler Greenup <Tyler.Greenup@Concentrus.com>
2017-07-25 15:37:42 -0700

    Start building statistics

--- statistics.py
+++ statistics.py
@@ -1,22 +1,43 @@
 """Statistics module
 """
+from experience import Experience
+
+NATURES = {'serious' : (0, 0)}
 
 class Statistics(Experience):
 
-    def __init__(self, base_stats, nature, evs=[0,0,0,0,0,0], ivs=[0,0,0,0,0,0]):
-        # Initialize
+    def __init__(self, nature='serious', stats=None, base=(0, 0, 0, 0, 0, 0),
+                 evs=(0, 0, 0, 0, 0, 0), ivs=(0, 0, 0, 0, 0, 0), exp_group='slow',
+                 current_exp=0):
 
-    def set_stats(self, base_stats, evs, ivs):
-        # Set self.stats to a list of current set_stats
+        super().__init__(exp_group=exp_group, current_exp=current_exp)
 
-    def set_evs(self, evs):
-        # Set ev list to new ev list, run set_stats at end
+        if stats is None:
+            self.stats = get_stats(base, evs, ivs)
+        else:
+            self.stats = stats
 
-    def ivs(self, ivs):
-        # Set iv list to new iv list, run set_stats at end
+        self.base = base
+        self.evs = evs
+        self.ivs = ivs
 
-    def set_nature(self, nature):
-        # Set nature to new nature, then set_stats
+    # def get_stats(self, base, evs, ivs):
+    #     # Set self.stats to a list of current set_stats
+    #
+    # def set_evs(self, evs):
+    #     # Set ev list to new ev list, run set_stats at end
+    #
+    # def ivs(self, ivs):
+    #     # Set iv list to new iv list, run set_stats at end
+    #
+    # def set_nature(self, nature):
+    #     # Set nature to new nature, then set_stats
+    #
+    # def nature_effect(self, nature):
+    #     # Applies nature impact to stats, to be run in set_stats
 
-    def nature_effect(self, nature):
-        # Applies nature impact to stats, to be run in set_stats
+def value_check(nature='serious', stats=None, base=(0, 0, 0, 0, 0, 0),
+                evs=(0, 0, 0, 0, 0, 0), ivs=(0, 0, 0, 0, 0, 0)):
+    if nature not in NATURES:
+        raise ValueError("nature must be one of {}, {} was given".format(
+            NATURES.keys(), nature))
