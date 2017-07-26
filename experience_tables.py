bb1ab2e26a9389a6af683ea5db4ec523e99fe05d
Tyler Greenup <Tyler.Greenup@Concentrus.com>
2017-07-26 10:51:45 -0700

    Clean out old code and combine methods that do basically the same thing.

--- experience_tables.py
+++ experience_tables.py
@@ -22,8 +22,6 @@ def build_tables():
         # Conditionals necessary for erratic and fluctuating.
 
         if level <= 50:
-            # exp_tables['erratic'][int(\
-            #     (1/50) * ((level ** 3) * (100 - level)))] = level
             exp_tables['erratic'][level] = int(\
                 (1/50) * ((level ** 3) * (100 - level)))
 
@@ -38,57 +36,24 @@ def build_tables():
                 level ** 3 * (((level / 2) + 32) / 50))
 
         elif level <= 68:
-            # exp_tables['erratic'][int(\
-            #     (1/100) * ((level ** 3) * (150 - level)))] = level
             exp_tables['erratic'][level] = int(\
                 (1/100) * ((level ** 3) * (150 - level)))
 
             exp_tables['fluctuating'][level] = int(\
                 level ** 3 * (((level / 2) + 32) / 50))
-                
+
         elif level <= 98:
-            # exp_tables['erratic'][int(\
-            #     (1/500) * ((level ** 3) * ((1911 - 10 * level) / 3)))] = level
             exp_tables['erratic'][level] = int(\
                 (1/500) * ((level ** 3) * ((1911 - 10 * level) / 3)))
 
             exp_tables['fluctuating'][level] = int(\
                 level ** 3 * (((level / 2) + 32) / 50))
+
         else:
-            # exp_tables['erratic'][int(\
-            #     (1/100) * ((level ** 3) * (160 - level)))] = level
             exp_tables['erratic'][level] = int(\
                 (1/100) * ((level ** 3) * (160 - level)))
 
             exp_tables['fluctuating'][level] = int(\
                 level ** 3 * (((level / 2) + 32) / 50))
 
-    # #fast
-    # for level in range(2, 102):
-    #     exp_tables['fast'][int((4 * level ** 3) / 5)] = level
-    #
-    # #medium_fast
-    # for level in range(2, 102):
-    #     exp_tables['medium_fast'][int(level ** 3)] = level
-    #
-    # #medium_slow
-    # for level in range(2, 102):
-    #     exp_tables['medium_slow'][int(\
-    #         ((6 / 5) * level ** 3) - (15 * level ** 2) + (100 * level) - 140)] = level
-    #
-    # #slow
-    # for level in range(2, 102):
-    #     exp_tables['slow'][int((5 * level ** 3) / 4)] = level
-    #
-    # #fluctuating
-    # for level in range(2, 102):
-    #     if level <= 15:
-    #         exp_tables['fluctuating'][int(level ** 3 * \
-    #             ((((level + 1) / 3) + 24) / 50))] = level
-    #     elif level <= 36:
-    #         exp_tables['fluctuating'][int(level ** 3 * \
-    #             ((level + 14) / 50))] = level
-    #     else:
-    #         exp_tables['fluctuating'][int(level ** 3 * \
-    #             (((level / 2) + 32) / 50))] = level
         return exp_tables
