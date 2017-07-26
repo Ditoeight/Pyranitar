ca456edef185074ec4829c3ff234647ce98953a7
Tyler Greenup <Tyler.Greenup@Concentrus.com>
2017-07-26 16:31:13 -0700

    Use pandas instead of CSV for table creation

--- Tables/build_db.py
+++ Tables/build_db.py
@@ -1,24 +1,44 @@
 import sqlite3
-import csv
 import os
 import pandas as pd
 
-path = os.path.dirname(__file__)+"/"
+TABLES = [['Natures', 'nature'],
+          ['Experience'],
+         ]
 
-filename = path + 'Natures.csv'
+PATH = os.path.dirname(__file__)+"/"
+CONNECTION = sqlite3.connect(PATH + 'serpyrior.db')
 
-conn = sqlite3.connect(path + 'serpyrior.db')
-cur = conn.cursor()
-cur.execute("CREATE TABLE IF NOT EXISTS natures(nature, hp, atk, def, spa, spd, spe)")
-filename.encode('utf-8')
-with open(filename) as f:
-    reader = csv.reader(f)
-    for field in reader:
-        cur.execute("INSERT INTO natures VALUES (?,?,?,?,?,?,?);", field)
+# insert a little jimmy drop tables here
 
-conn.commit()
 
-df = pd.read_sql_query("SELECT * FROM natures", conn, index_col='nature')
+for table in TABLES:
+    table_name = table[0]
+    print(table_name)
+    try:
+        table_index = table[1]
+        write_index = False
+    except IndexError:
+        table_index = None
+        write_index = True
 
-print(df.head(25))
-conn.close()
+    df = pd.read_csv(PATH + table_name + '.csv')
+    df.to_sql(table_name, CONNECTION, index=write_index, index_label=table_index)
+
+CONNECTION.commit()
+CONNECTION.close()
+
+# cur = conn.cursor()
+# cur.execute("CREATE TABLE IF NOT EXISTS natures()")
+# filename.encode('utf-8')
+# with open(filename) as f:
+#     reader = csv.reader(f)
+#     for field in reader:
+#         cur.execute("INSERT INTO natures VALUES (?,?,?,?,?,?,?);", field)
+#
+# conn.commit()
+#
+# df = pd.read_sql_query("SELECT * FROM natures", conn, index_col='nature')
+#
+# print(df.head(25))
+# conn.close()
