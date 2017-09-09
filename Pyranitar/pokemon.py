20e697e0d5e3c37d6cd33772c27be589b5e31158
Ditoeight <Ditoeight@gmail.com>
2017-09-08 19:33:20 -0700

    Flesh out more of the attributes

--- Pyranitar/pokemon.py
+++ Pyranitar/pokemon.py
@@ -3,9 +3,16 @@
 """Pokemon module
 """
 
+from statistics import Statistics
+from db_queries import query_experience_yield
+
 class Pokemon(Statistics):
 
-    def __init__(self, index):
-        # Initialize
+    def __init__(self, dex_no, form=None, nature='serious', evs=(0, 0, 0, 0, 0, 0),
+                 ivs=(0, 0, 0, 0, 0, 0), current_exp=None, level=100, hold_item=None,
+                 original_trainer=True, ):
+        super().__init__(dex_no=dex_no, form=form, nature=nature, evs=evs,
+                         ivs=ivs, current_exp=current_exp, level=level)
 
-    #Other methods tbd
+    def defeat(self, enemy_dex, enemy_level, form=None):
+        pass
